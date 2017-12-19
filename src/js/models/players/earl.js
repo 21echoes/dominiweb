9dca74aee96f85e7aeb5bb64bc24aec75b186c3f
David Kettler <21echoes@gmail.com>
2017-12-19 22:35:13 +0800

    fix a bug in Earl where you could take Earl’s turn if he had an action in hand. for now, just fix the bug, but eventually we’ll want Earl to play actions somewhat intelligently.

--- src/js/models/players/earl.js
+++ src/js/models/players/earl.js
@@ -1,4 +1,4 @@
-define(['backbone', 'models/players/player'], function(Backbone, Player) {
+define(['backbone', 'models/players/player', 'models/cards/cards'], function(Backbone, Player, Cards) {
   return Player.extend({
     key: 'earl',
 
@@ -14,8 +14,9 @@ define(['backbone', 'models/players/player'], function(Backbone, Player) {
 
     turnChanged: function(turn) {
       if (turn.playState() == 'ACTIONS') {
-        // TODO: check for action_resolution
-        // else, check for playable actions, choose one, play it, rinse & repeat
+          turn.advancePlayState();
+        // TODO: check for action_resolution, resolve intelligently if so
+        // TODO: finish and use this.playBestAction(turn);
       } else if (turn.playState() == 'TREASURES') {
         turn.takeTurnAction('play-all-treasures');
       } else if (turn.playState() == 'BUY') {
@@ -56,5 +57,83 @@ define(['backbone', 'models/players/player'], function(Backbone, Player) {
       }
       return null;
     },
+
+    playBestAction: function(turn) {
+        var bestAction = this.chooseBestActionToPlay(turn);
+        if (bestAction) {
+            turn.playAction(bestAction);
+        }
+    },
+
+    chooseBestActionToPlay: function(turn) {
+        var hand = turn.get('player').get('hand');
+
+        var actionCards = hand.find_cards_by_type('action');
+        if (actionCards.length === 0) {
+            return null;
+        }
+
+        var dontPlayTypes = new Cards(['rats']);
+        // TODO: don't play tactician if tactician already in play
+        // TODO: don't play throne room if it's gonna make you play something in dontPlay
+
+        // play a treasure map if you have 2 or more in hand
+        if (hand.find_cards_by_key('treasure-map').length >= 2) {
+            return hand.find_cards_by_key('treasure-map')[0];
+        }
+        dontPlay.add('treasure-map');
+
+        if (hand.find_cards_by_key('throne-room').length > 0) {
+            return hand.find_cards_by_key('throne-room')[0];
+        }
+
+        var bestEngineAction = this.getBestEngineAction(hand);
+        if (bestEngineAction != null) {
+          return bestEngineAction;
+        }
+
+        if (hand.find_cards_by_key('mine').length > 0 && this.mineableCards(hand) > 0) {
+          return hand.find_cards_by_key('mine')[0];
+        }
+        dontPlay.add('mine');
+
+        if (hand.find_cards_by_key('money-lender').length > 0
+            && hand.find_cards_by_key('copper').length > 0) {
+          return hand.find_cards_by_key('money-lender')[0];
+        }
+        dontPlay.add('money-lender');
+
+        var preferenceOrder = [
+            'sea-hag',
+            'cutpurse',
+            'militia',
+            'bureaucrat',
+            'library'
+        ];
+        for (var i = 0; i < preferenceOrder.length; i++) {
+            var key = preferenceOrder[i];
+            if (hand.find_cards_by_key(key).length > 0) {
+              return hand.find_cards_by_key(key)[0];
+            }
+        }
+
+        // TODO: sort by... cost?
+        for (var i = 0; i < actionCards.length; i++) {
+            var card = actionCards[i];
+            if (dontPlay.indexOf(card.get('key')) !== -1) {
+                continue;
+            }
+            return card;
+        }
+        return null;
+    },
+
+    getBestEngineAction: function(hand) {
+        // TODO: need way to 'statically' analyze the cards in hand for how much they'll boost action count
+        // if one exists, return the best one
+
+        // TODO: need way to 'statically' analyze the cards in hand for how much they'll boost draw count
+        // if one exists, return the best one
+    }
   });
 });
